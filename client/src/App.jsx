0fc3c92a345e936d55df0b3581cb4ff14a5de9f4
Sachin Tom <sachintomacc@gmail.com>
2023-10-31 21:39:05 +0530

    updated client query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
 import { gql, useQuery } from "@apollo/client"
 
 const GET_CHALLENGES = gql`
-    query getAllChallenges {
-        question
-        answer
-        options
+    query ExampleQuery {
+        getAllChallenges {
+            question
+            answer
+            options
+        }
     }
 `
 
 function App() {
     const { loading, error, data } = useQuery(GET_CHALLENGES)
-    console.log(data, "====")
-    console.log(error);
+    console.log(error)
 
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error : {error.message}</p>
 
-    return <div>qwewe</div>
+    console.log(data.getAllChallenges)
+    return <p>qwe</p>
 }
 
 export default App
